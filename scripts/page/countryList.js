70b1da54361d37e17e1c631afae38b838e01a07b
ryanny <ryannymromero@gmail.com>
2024-07-08 00:20:56 -0800

    Finish Show By Country

--- scripts/page/countryList.js
+++ scripts/page/countryList.js
@@ -2,7 +2,7 @@ import { fetchMovieData } from '../../data/fetchMovie.js';
 
 async function renderCountryList() {
   const countryList = await fetchMovieData('configuration/countries');
-  const list = ['United States of America', 'united kingdom', 'canada', 'brazil', 'new zealand', 'france', 'spain', 'australia', 'ireland', 'italy', 'japan', 'switzerland', 'denmark', 'belgium'];
+  const list = ['United States of America', 'united kingdom', 'canada', 'brazil', 'new zealand', 'france', 'spain', 'australia', 'ireland', 'italy', 'japan', 'switzerland', 'denmark', 'belgium', 'philippines'];
   const filteredList = list.map(country => {
     return countryList.find(c => country.toLowerCase() === c.english_name.toLowerCase());
   });
@@ -10,7 +10,7 @@ async function renderCountryList() {
   const countryBtn = filteredList.map(country => {
     return `
       <button class="px-5 py-2 bg-gray-700 rounded-full duration-300 hover:bg-gray-600" data-country-code="${country.
-        iso_3166_1}">${country.native_name}</button>
+        iso_3166_1}" data-country="${country.native_name}">${country.native_name}</button>
     `;
   }).join('')
 
@@ -19,9 +19,9 @@ async function renderCountryList() {
   const countryBtnsEl = document.querySelectorAll('.country-btns button');
   countryBtnsEl.forEach(btn => {
     btn.addEventListener('click', () => {
-      const { countryCode } = btn.dataset;
+      const { countryCode, country } = btn.dataset;
 
-      window.location.href = `./show-by-country.html?country=${countryCode}`;
+      window.location.href = `./show-by-country.html?country=${country}&country-code=${countryCode}&page=1`;
     })
   })
 };
--- scripts/page/showByCountry.js
+++ scripts/page/showByCountry.js
@@ -1,27 +1,25 @@
-import { fetchMovieData } from "../../data/fetchMovie.js";
+import { fetchMovieData } from '../../data/fetchMovie.js';
 import { controlPage } from '../utils/pageControl.js';
-import { formatDate, formatVote, formatRunTime } from "../utils/formatDate.js";
+import { formatVote, formatRunTime } from '../utils/formatDate.js';
 
-function showByGenre() {
-  renderShow();
-  
-}
-
-async function renderShow() {
+function renderShowByCountry() {
   const url = new URL(window.location.href);
   const params = url.searchParams;
-  const genreName = params.get('genre-name');
-  const genreId = params.get('genre-id');
+  const country = params.get('country');
+  const countryCode = params.get('country-code');
   const pageNumber = params.get('page');
-  const showType = params.get('show-type');
-  const isSeries = showType === 'tv';
+  
+  showByCountry(country, countryCode, pageNumber);
+}
 
-  const movieData = await fetchMovieData(`discover/${isSeries?'tv':'movie'}?include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=popularity.desc&with_genres=${genreId}`);
-  const movieResult = movieData.results;
-  const showWrapperHTML = await Promise.all(movieResult.map(async show => {
-    const detail = await fetchMovieData(`${isSeries ? 'tv' : 'movie'}/${show.id}`)
-    const title = isSeries ? show.name : show.title;
-    const runtime = isSeries ? `S ${detail.number_of_seasons}/EP ${detail.number_of_episodes}` : formatRunTime(detail.runtime);
+async function showByCountry(country, countryCode, pageNumber) {
+  const movieData = await fetchMovieData(`discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_origin_country=${countryCode}`);
+  const movieList = movieData.results;
+
+  const showWrapperHTML = await Promise.all(movieList.map(async show => {
+    const detail = await fetchMovieData(`movie/${show.id}`)
+    const title = show.title;
+    const runtime = formatRunTime(detail.runtime);
 
     return `
     <div class="w-full rounded-md bg-gray-700">
@@ -41,11 +39,10 @@ async function renderShow() {
   }));
   
   document.querySelector('.show-container').innerHTML = showWrapperHTML.join('');
-  document.querySelector('.main-title h1').textContent = genreName;
+  document.querySelector('.main-title h1').textContent = country;
   controlPage(pageNumber);
 }
 
-
 window.addEventListener('load', () => {
-  showByGenre();
-});
+  renderShowByCountry();
+})
